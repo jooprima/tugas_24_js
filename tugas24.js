03b50ee8e39f145c0a75db3c7b477a4ceaf03e40
yohanes_prima <jooprima@gmail.com>
2019-07-14 23:27:09 +0700

    Tugas 24 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas24.js
@@ -0,0 +1,6 @@
+var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log("sebelumnya : " + angka);
+
+var angka2 = angka.sort();
+console.log("Ascending : " + angka2);
+console.log("Descending : " + angka2.reverse());
